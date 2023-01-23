8b79f2220439b28ee942cdd6a1eb4663ab76b3b8
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2023-01-23 17:56:12 +0100

    add report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
+import { ReportsModule } from './reports/reports.module';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
